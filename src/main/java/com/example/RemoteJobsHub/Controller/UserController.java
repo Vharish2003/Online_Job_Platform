c692309a1cb88441b0c5172ff7cf5fa60f3dda08
Harish V <vaddiharish2003@gmail.com>
2025-04-23 17:33:57 +0530

    job search pltform

--- src/main/java/com/example/RemoteJobsHub/Controller/UserController.java
+++ src/main/java/com/example/RemoteJobsHub/Controller/UserController.java
@@ -1,7 +1,15 @@
 package com.example.RemoteJobsHub.Controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +30,42 @@ public class UserController {
 	public User saveUser(@RequestBody User users) {
 		return userService.saveUsers(users);
 	}
+	
+	
+	@GetMapping("All")
+	public ResponseEntity<?>getAll(){
+		List<User>users=userService.getAllUsers();
+		if(users.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Given body is Empty");
+		}
+		else {
+			return ResponseEntity.ok(users);
+		}
+	}
+	
+	
+	@GetMapping("/{id}")
+		public ResponseEntity<?>getByid(@PathVariable Long id){
+			Optional<User>users=userService.getById(id);
+			if(users.isPresent()) {
+				return ResponseEntity.ok(users.get());
+						
+			}
+			else {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Given Id is Not Exists"+id);
+			}
+		}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?>deleteByid(@PathVariable Long id){
+		boolean users=userService.deleteById(id);
+		if(users) {
+			return ResponseEntity.ok("deleted Successfully");
+		}
+		else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("given id is not deleted");
+		}
+	}
+	}
 
-}
+
