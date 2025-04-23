5e6930e121903d9e446980be788788c72cebacfe
Harish V <vaddiharish2003@gmail.com>
2025-04-23 17:16:32 +0530

    job search pltform

--- src/main/java/com/example/RemoteJobsHub/Controller/UserController.java
+++ src/main/java/com/example/RemoteJobsHub/Controller/UserController.java
@@ -1,5 +1,26 @@
 package com.example.RemoteJobsHub.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.RemoteJobsHub.Entity.User;
+import com.example.RemoteJobsHub.Service.UserService;
+
+@Controller
+@RestController("/api/users")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	
+	@PostMapping("Save")
+	public User saveUser(@RequestBody User users) {
+		return userService.saveUsers(users);
+	}
 
 }
